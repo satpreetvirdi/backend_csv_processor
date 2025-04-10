3a2568f2e3cd77a58fc6cea0aa9423abe5793365
Satpreet Singh Virdi <sanjanagulrajani@Satpreets-MacBook-Air.local>
2025-04-11 01:05:37 +0530

    fixed : added json postman

--- routes/downloadCsv.js
+++ routes/downloadCsv.js
@@ -1,24 +1,19 @@
 const express = require('express');
-const fs = require('fs');
-const path = require('path');
+
 const json2csv = require('json2csv').parse;
 const supabase = require('../supaBaseclient');
-const Request = require('../models/Request');
+
 const router = express.Router();
 const Image = require('../models/Image');
 
 router.get('/:requestId', async (req, res) => {
   try {
     console.log('Request ID:', req.params.requestId);
-    // const request = await Request.findOne({ requestId: req.params.requestId });
-    // console.log('Request:', request);
-    // if (!request) {
-    //   return res.status(402).json({ error: 'Request not found' });
-    // }
+
 
     const images = await Image.find({ requestId: req.params.requestId });
 
-    // Prepare CSV data
+
     const csvData = images.map(img => {
       return img.inputUrls.map((url, index) => ({
         'Request ID': req.params.requestId,
@@ -28,29 +23,29 @@ router.get('/:requestId', async (req, res) => {
       }));
     }).flat();
 
-    // Convert the data to CSV format
+
     const csv = json2csv(csvData);
     const fileName = `request-${req.params.requestId}.csv`;
 
-    // Upload the CSV file to Supabase Storage
+
     const { data, error } = await supabase
       .storage
-      .from('csvstorage') // Make sure your bucket name is correct
+      .from('csvstorage') 
       .upload(fileName, Buffer.from(csv), {
-        cacheControl: '3600', // Set cache control to 1 hour
-        upsert: true, // If the file already exists, overwrite it
+        cacheControl: '3600', 
+        upsert: true, 
       });
 
     if (error) {
       throw error;
     }
 
-    // Get the public URL of the uploaded file
+
     const publicUrl = await supabase.storage
       .from('csvstorage')
       .getPublicUrl(fileName)
     console.log('Public URL:', publicUrl);
-    // Return the public URL to the client
+
     res.json({ fileUrl: publicUrl });
 
   } catch (error) {
